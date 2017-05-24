07ee5c6a66bc398feed05d63762c35e77439a795
Guven Kemal <guven@lindar.com>
2017-05-24 11:14:28 +0100

    further tweaks to game options form

--- src/Header/index.js
+++ src/Header/index.js
@@ -8,7 +8,7 @@ import './header.css';
 class Header extends PureComponent {
   constructor() {
     super();
-    this.state = { _numOfPlayers: null, _numOfCards: null, error: false, errorMsg: null, optionsSubmitted: false };
+    this.state = { _numOfPlayers: 0, _numOfCards: 0, error: false, errorMsg: null, optionsSubmitted: false };
   }
 
   componentWillMount() {
@@ -17,28 +17,22 @@ class Header extends PureComponent {
 
   updateNumOfPlayers = (e) => {
     this.setState({ error: false, errorMsg: null, optionsSubmitted: false});
-    const _numOfPlayers = +e.target.value;
+    const _numOfPlayers = +e.target.value > 26 ? 26 : +e.target.value;
     this.setState({ _numOfPlayers });
     this.validateForm(_numOfPlayers, this.state._numOfCards);
   }
 
   updateNumOfCards = (e) => {
     this.setState({ error: false, errorMsg: null, optionsSubmitted: false});
-    const { _numOfPlayers } = this.state;
-    const _numOfCards = +e.target.value;
+    const _numOfCards = +e.target.value > 52 ? 52 : +e.target.value;
     this.setState({ _numOfCards });
-    this.validateForm(_numOfPlayers, _numOfCards);
+    this.validateForm(this.state._numOfPlayers, _numOfCards);
   }
 
   validateForm = (numOfPlayers, numOfCards) => {
     const maxCardsAvailable = Math.floor(52 / numOfPlayers);
-    if (numOfPlayers > 26) {
-      this.setState({ error: true, errorMsg: 'Maximum Amount of players is 26'});
-    } else if (numOfPlayers < 2) {
+    if (numOfPlayers < 2) {
       this.setState({ error: true, errorMsg: 'Minimum Amount of players is 2'});
-    }
-    if (numOfCards > 52) {
-      this.setState({ error: true, errorMsg: 'Maximum Amount of cards available is is 52'});
     }
     if (numOfCards > maxCardsAvailable) {
       this.setState({
@@ -71,11 +65,11 @@ class Header extends PureComponent {
         <div className="input-options">
           <div className="game-options-form players">
             <label htmlFor="choose-players-input">Choose Number Of Players</label>
-            <input type="number" id="choose-players-input" onChange={this.updateNumOfPlayers} min="2" max="26"/>
+            <input type="number" id="choose-players-input" value={_numOfPlayers} onChange={this.updateNumOfPlayers} min="2" max="26"/>
           </div>
           <div className={gameOptionsFormCards}>
             <label htmlFor="choose-cards-input">Choose Number Of Cards To Be Dealt</label>
-            <input type="number" id="choose-cards-input" onChange={this.updateNumOfCards} min="1" max="52"/>
+            <input type="number" id="choose-cards-input" value={_numOfCards} onChange={this.updateNumOfCards} min="1" max="52"/>
           </div>
           <button
             disabled={submitDisabled}
