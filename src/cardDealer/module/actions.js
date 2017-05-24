f694a57e0f02a8f48f624d17ce9ca1e80b19140a
Guven Kemal <guven@lindar.com>
2017-05-24 20:59:17 +0100

    shuffle deck before dealing cards

--- src/cardDealer/module/actions.js
+++ src/cardDealer/module/actions.js
@@ -1,22 +1,18 @@
 import * as a from './actionTypes';
 import { getDealtCards, determineWinner } from './utils';
 
-const shuffleDeck = () => ({
-  type: a.SHUFFLE_DECK
-});
-
-const getWinner = ({ dealtCards } = {}) => ({
-  type: a.GET_WINNER,
-  winner: determineWinner(dealtCards)
-});
-
 export const dealCards = ({deckOfCards, numOfPlayers, numOfCards} = {}) =>
   dispatch => {
+    dispatch({
+      type: a.SHUFFLE_DECK
+    });
     const dealtCards = getDealtCards(deckOfCards, numOfPlayers, numOfCards);
     dispatch({
       type: a.DEAL_CARDS,
       dealtCards
     });
-    dispatch(shuffleDeck())
-    dispatch(getWinner({ dealtCards }));
+    dispatch({
+      type: a.GET_WINNER,
+      winner: determineWinner(dealtCards)
+    });
   };
